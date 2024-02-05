ba63deca0c31e546791a5619459c1ab48b4aaf1e
lxhoanghsgs <lxhoanghsgs@gmail.com>
2024-02-05 09:51:03 +0700

    Completed Example 4.10, predicted the temperature in April 2 using conditional distributions.

--- example_4_10.r
+++ example_4_10.r
@@ -1,4 +1,5 @@
 # My task: modify the method in Example 4.10 to predict the temperature in April 2, which is also Prob. no. 6 (page 269).
+library(matlib)
 temp_in_march <- read.csv(r"(C:\StatWork\advancedstatistics-master\RcodeData\foold.csv)")
 mu <- vector(mode = "integer", length = 32)
 mu <- replace(mu, mu == 0, 33)
@@ -13,6 +14,10 @@ cov_entries <- function(i, j){
 }
 rows <- c(1:31, 33)
 cols <- c(1:31, 33)
-
+# We need 33 because we want to predict the temperature in April 2.
 cov_matrix <- outer(rows, cols, FUN = cov_entries)
 # plot(temp_in_march$Day, temp_in_march$Temperature, type = "b")
+# Use conditional distribution.
+prediction_april_2 <- mu[32] + cov_matrix[32, 1:31] %*% solve(cov_matrix[1:31, 1:31]) %*% (temp_in_march$Temperature - mu[1:31])
+prediction_april_2
+# prediction_april_2 = 31.72. Done!
--- sample.r
+++
@@ -1,5 +0,0 @@
-twoprod <- function(t1, t2) {
-  prod <- t1 * t2
-  return(prod)
-}
-twoprod(t1 = 2, t2 = 3)
